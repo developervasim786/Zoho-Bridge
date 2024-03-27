f4a1b2aca8172aa225ed9a9ad2eb063414df341e
developervasim786 <153063345+developervasim786@users.noreply.github.com>
2024-03-27 13:24:08 +0530

    Add files via upload

--- inc/actions.php
+++ inc/actions.php
@@ -1,1 +1,30 @@
+<?php
+/**
+ * Plugin main actions file.
+ * 
+ * @since   2.0.0
+ */
 
+// Lets check lead check
+
+add_action("init",function(){
+
+    if( isset( $_GET["sample_lead"] ) ){
+        $data = array(
+            "data" => array(
+                array(
+                    "First_Name"    => "Developer",
+                    "Last_Name"     => "Rajesh"
+                )
+            )
+        );
+
+        $lead = zcrm_create_record( "Leads", $data );
+
+        echo "<pre>";
+        print_r($lead);
+        echo "</pre>";
+        die;
+    }
+},999);
+?>
